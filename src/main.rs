149d1ad374fa2b0d9d8b3e431055cbc574e406b7
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2023-06-10 11:46:41 +0530

    Update main.rs
    rust second update in this repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::io::Write;
-//Author Prince Kumar 
+use std::env::args;
+//Author Prince Kumar
 //date 9 jun 2023
 //make a function to write string to the file
 fn writefile(pass: String){
@@ -8,8 +9,24 @@ fn writefile(pass: String){
    file.write_all(pass.as_bytes()).expect("write failed");
 }
 fn main() {
+    // create an empty vector to keep track of the pass
+    let mut passlist: Vec<u128> = Vec::new();
+    // Now collect the args into a vector
+    let arguments:Vec<String> = args().collect();
     let mut rng = rand::thread_rng();
-    let rand_num:u128 = rng.gen_range(0..100);
-    writefile(rand_num.to_string());
-    println!("Hello, world!");
+    for i in 1..=arguments[3].parse().unwrap(){
+    let rand_num:u128 = rng.gen_range(arguments[1].parse().unwrap()..arguments[2].parse().unwrap());
+    if i == 1
+    {continue;}else{
+                        passlist.push(rand_num);
+                            for pass in passlist.iter(){
+                                if *pass == rand_num {
+                                continue;}else{
+                                    println!("{} {}",rand_num,pass);
+                                    writefile(rand_num.to_string());
+                                }
+
+        }
+    }}
+
 }
